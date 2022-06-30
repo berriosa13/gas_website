ccac64db9a1f0dc7eb7e8932d12e93ea2aabadba
berriosa13 <berriosa13@gmail.com>
2022-06-30 16:03:51 -0400

    Removed quotes that were causing eslint errors

--- components/forms/QuoteForm.js
+++ components/forms/QuoteForm.js
@@ -117,7 +117,7 @@ function QuoteForm( { setCar }) {
         </Row>
         <Row className="m-3">
           <Col>
-            <p className="">By clicking "SEND", I consent to be contacted by <em>Guardian Automobile Sales</em> at any phone number or email I provide, including, without limitation, communications sent via text message to my cell phone or communications sent using an autodialer or prerecorded message. This acknowledgment constitutes my written consent to receive such communications. I have read and agree to the Privacy Policy of <em>Guardian Automobile Sales</em>.</p>
+            <p className="">By clicking SEND, I consent to be contacted by <em>Guardian Automobile Sales</em> at any phone number or email I provide, including, without limitation, communications sent via text message to my cell phone or communications sent using an autodialer or prerecorded message. This acknowledgment constitutes my written consent to receive such communications. I have read and agree to the Privacy Policy of <em>Guardian Automobile Sales</em>.</p>
           </Col>
         </Row>
         <Row className="text-center m-3">
