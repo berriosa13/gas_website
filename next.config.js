e55e2274726aa6f6347335fe9907b50b1a4684da
berriosa13 <berriosa13@gmail.com>
2022-06-30 09:46:04 -0400

    edited nextjs config file to allow html file to be loaded for privacy policy

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: { domains: ['firebasestorage.googleapis.com'], },
+
+  rewrites: async () => [
+    {
+      source: "/public/privacy_policy.html",
+      destination: "/pages/api/privacyPolicyFile.js",
+    },
+  ],
   
 }
 
