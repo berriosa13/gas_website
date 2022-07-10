c9c2b180df3162821393d9e3df00f1e243cf935b
Anthony Berrios <54926487+berriosa13@users.noreply.github.com>
2022-07-09 20:55:13 -0400

    removed dollar sign typo from carDetails page

--- pages/carDetails.js
+++ pages/carDetails.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
 import { useRouter } from "next/router";
 import styles from "../styles/page_styles/Cars.module.css";
 import utilMethods from "../services/utils";
-import ImageModal  from "../components/modals/ImageModal";
-import QuoteModal from "../components/modals/QuoteModal"
-import AvailabilityModal from "../components/modals/AvailabilityModal"
-import TestDriveModal from "../components/modals/TestDriveModal"
+import ImageModal from "../components/modals/ImageModal";
+import QuoteModal from "../components/modals/QuoteModal";
+import AvailabilityModal from "../components/modals/AvailabilityModal";
+import TestDriveModal from "../components/modals/TestDriveModal";
 import { BsDashLg } from "react-icons/bs";
 
 import {
@@ -47,11 +47,11 @@ export default function CarDetails() {
     };
     retrieveImages();
   }, [car.id]);
-  
+
   const showImageModal = () => {
     setImageModalOpen(true);
   };
-  
+
   const hideImageModal = () => {
     setImageModalOpen(false);
   };
@@ -59,7 +59,7 @@ export default function CarDetails() {
   const showQuoteModal = () => {
     setQuoteModalOpen(true);
   };
-  
+
   const hideQuoteModal = () => {
     setQuoteModalOpen(false);
   };
@@ -67,7 +67,7 @@ export default function CarDetails() {
   const showAvailabilityModal = () => {
     setAvailabilityModalOpen(true);
   };
-  
+
   const hideAvailabilityModal = () => {
     setAvailabilityModalOpen(false);
   };
@@ -75,34 +75,51 @@ export default function CarDetails() {
   const showTestDriveModal = () => {
     setTestDriveModalOpen(true);
   };
-  
+
   const hideTestDriveModal = () => {
     setTestDriveModalOpen(false);
   };
-  
-  
+
   return (
     <>
-      
-      <ImageModal show={imageModalOpen} handleClose={hideImageModal} setImages={images} setCar={car} />
-      <QuoteModal show={quoteModalOpen} handleClose={hideQuoteModal} setCar={car} />
-      <AvailabilityModal show={availabilityModalOpen} handleClose={hideAvailabilityModal} setCar={car} />
-      <TestDriveModal show={testDriveModalOpen} handleClose={hideTestDriveModal} setCar={car} />
-         
+      <ImageModal
+        show={imageModalOpen}
+        handleClose={hideImageModal}
+        setImages={images}
+        setCar={car}
+      />
+      <QuoteModal
+        show={quoteModalOpen}
+        handleClose={hideQuoteModal}
+        setCar={car}
+      />
+      <AvailabilityModal
+        show={availabilityModalOpen}
+        handleClose={hideAvailabilityModal}
+        setCar={car}
+      />
+      <TestDriveModal
+        show={testDriveModalOpen}
+        handleClose={hideTestDriveModal}
+        setCar={car}
+      />
+
       <Head>
         <title>GAS Automobile Sales | Car Details</title>
         <meta name="keywords" content="cars" />
       </Head>
 
       <div className="d-flex my-5 justify-content-between">
-        <h1>Used {car.year} {car.make} {car.model}</h1>
-          <Breadcrumb className="fst-italic mx-3">
+        <h1>
+          Used {car.year} {car.make} {car.model}
+        </h1>
+        <Breadcrumb className="fst-italic mx-3">
           <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
           <Breadcrumb.Item href="/cars">Inventory</Breadcrumb.Item>
           <style jsx global>{`
             a {
               color: var(--main-color) !important;
-            } 
+            }
             a:hover {
               color: var(--secondary-color) !important;
             }
@@ -111,102 +128,120 @@ export default function CarDetails() {
             }
           `}</style>
         </Breadcrumb>
-      </div> 
-            
+      </div>
+
       <main>
         <section className={styles.featured_places}>
           <div className="container mt-5">
             <div className="row">
               <section id="photoArray">
-                
-                  {car.thumbnailImage != null && displayImages != null ? (
-                    <>
-                      <div>
-                        <Image
-                          layout="responsive"
-                          width={600}
-                          height={500}
-                          alt="thumbnailImage"
-                          src={car.thumbnailImage}
-                        />
-                      </div>
-                       {displayImages.map((displayImage) => {
-                        return(
-                          <div key={displayImage.id} style={{  position: 'relative', width: 'auto', height: 'auto'}}>
-                            <Image
-                              priority="true"
-                              src={displayImage.imageUrl}
-                              layout="fill"
-                              objectFit="unset"
-                              alt="displayImages"
-                            />
-                          </div>
-                          );
-                        })}
-                    
-                    </>
-                    )
-                    : 
-                    (
-                      <>
-                      <div>
-                        <Image
-                          priority="true"
-                          src="https://via.placeholder.com/600x500.png?text=Image+Coming+Soon..."
-                          layout="responsive"
-                          alt="thumbnail image"
-                          width={600}
-                          height={500}
-                        />
-                      </div>
-                      <div style={{  position: 'relative', width: 'auto', height: 'auto'}}>
-                        <Image
-                          priority="true"
-                          src="https://via.placeholder.com/600x500.png?text=Image+Coming+Soon..."
-                          layout="fill"
-                          objectFit="unset"
-                          alt="thumbnail image"
-                          
-                          
-                        />
-                      </div>
-                      <div style={{  position: 'relative', width: 'auto', height: 'auto'}}>
-                        <Image
-                          priority="true"
-                          src="https://via.placeholder.com/600x500.png?text=Image+Coming+Soon..."
-                          layout="fill"
-                          objectFit="unset"
-                          alt="thumbnail image"
-                          
-                          
-                        />
-                      </div>
-                      <div style={{  position: 'relative', width: 'auto', height: 'auto'}}>
-                        <Image
-                          priority="true"
-                          src="https://via.placeholder.com/600x500.png?text=Image+Coming+Soon..."
-                          layout="fill"
-                          objectFit="unset"
-                          alt="thumbnail image"
-                          
-                          
-                        />
-                      </div>
-                      <div style={{  position: 'relative', width: 'auto', height: 'auto'}}>
-                        <Image
-                          priority="true"
-                          src="https://via.placeholder.com/600x500.png?text=Image+Coming+Soon..."
-                          layout="fill"
-                          objectFit="unset"
-                          alt="thumbnail image"
-                          
-                          
-                        />
-                      </div>
-                      </>
-                    )}
-                    
-             
+                {car.thumbnailImage != null && displayImages != null ? (
+                  <>
+                    <div>
+                      <Image
+                        layout="responsive"
+                        width={600}
+                        height={500}
+                        alt="thumbnailImage"
+                        src={car.thumbnailImage}
+                      />
+                    </div>
+                    {displayImages.map((displayImage) => {
+                      return (
+                        <div
+                          key={displayImage.id}
+                          style={{
+                            position: "relative",
+                            width: "auto",
+                            height: "auto",
+                          }}
+                        >
+                          <Image
+                            priority="true"
+                            src={displayImage.imageUrl}
+                            layout="fill"
+                            objectFit="unset"
+                            alt="displayImages"
+                          />
+                        </div>
+                      );
+                    })}
+                  </>
+                ) : (
+                  <>
+                    <div>
+                      <Image
+                        priority="true"
+                        src="https://via.placeholder.com/600x500.png?text=Image+Coming+Soon..."
+                        layout="responsive"
+                        alt="thumbnail image"
+                        width={600}
+                        height={500}
+                      />
+                    </div>
+                    <div
+                      style={{
+                        position: "relative",
+                        width: "auto",
+                        height: "auto",
+                      }}
+                    >
+                      <Image
+                        priority="true"
+                        src="https://via.placeholder.com/600x500.png?text=Image+Coming+Soon..."
+                        layout="fill"
+                        objectFit="unset"
+                        alt="thumbnail image"
+                      />
+                    </div>
+                    <div
+                      style={{
+                        position: "relative",
+                        width: "auto",
+                        height: "auto",
+                      }}
+                    >
+                      <Image
+                        priority="true"
+                        src="https://via.placeholder.com/600x500.png?text=Image+Coming+Soon..."
+                        layout="fill"
+                        objectFit="unset"
+                        alt="thumbnail image"
+                      />
+                    </div>
+                    <div
+                      style={{
+                        position: "relative",
+                        width: "auto",
+                        height: "auto",
+                      }}
+                    >
+                      <Image
+                        priority="true"
+                        src="https://via.placeholder.com/600x500.png?text=Image+Coming+Soon..."
+                        layout="fill"
+                        objectFit="unset"
+                        alt="thumbnail image"
+                      />
+                    </div>
+                    <div
+                      style={{
+                        position: "relative",
+                        width: "auto",
+                        height: "auto",
+                      }}
+                    >
+                      <Image
+                        priority="true"
+                        src="https://via.placeholder.com/600x500.png?text=Image+Coming+Soon..."
+                        layout="fill"
+                        objectFit="unset"
+                        alt="thumbnail image"
+                      />
+                    </div>
+                  </>
+                )}
+
                 <style jsx>{`
                   #photoArray {
                     display: grid;
@@ -239,30 +274,32 @@ export default function CarDetails() {
                   }
 
                   `}</style>
-                  
               </section>
               <div className="row my-3 d-flex justify-content-center text-center">
                 <div className="col-md-6 mt-3">
-                  <Button onClick={(e) => { setImageModalOpen(true)}} variant="primary">View All Images</Button>
+                  <Button
+                    onClick={(e) => {
+                      setImageModalOpen(true);
+                    }}
+                    variant="primary"
+                  >
+                    View All Images
+                  </Button>
                 </div>
               </div>
-              
+
               <div className="col">
                 <h2>
-                 {car.price ? 
-                  (
-                      <strong className="text-primary">
-                      $
-                      {car.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} 
-                    </strong>
-                  )
-                  :
-                  (
+                  {car.price ? (
                     <strong className="text-primary">
-                        Unavailable
-                      </strong>
-                  )
-                }
+                      $
+                      {car.price
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </strong>
+                  ) : (
+                    <strong className="text-primary">Unavailable</strong>
+                  )}
                 </h2>
 
                 <br />
@@ -305,28 +342,15 @@ export default function CarDetails() {
                   </ListGroup.Item>
 
                   <ListGroup.Item className="w-50 d-flex justify-content-end">
-                    {car.mileage ? 
-                      (
-                          <strong>
-                          $
-                          {car.mileage.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} 
-                        </strong>
-                      )
-                      :
-                      (
-                        <strong>
-                            Unavailable
-                          </strong>
-                      )
-                    }
-                  </ListGroup.Item>
-                </ListGroup>
-                <ListGroup horizontal>
-                  <ListGroup.Item className="w-50 d-flex justify-content-start">
-                    Fuel
-                  </ListGroup.Item>
-                  <ListGroup.Item className="w-50 d-flex justify-content-end">
-                    Diesel
+                    {car.mileage ? (
+                      <strong>
+                        {car.mileage
+                          .toString()
+                          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      </strong>
+                    ) : (
+                      <strong>Unavailable</strong>
+                    )}
                   </ListGroup.Item>
                 </ListGroup>
                 <ListGroup horizontal>
@@ -342,7 +366,7 @@ export default function CarDetails() {
                     Interior Color
                   </ListGroup.Item>
                   <ListGroup.Item className="w-50 d-flex justify-content-end">
-                    Black
+                    {car.interiorColor}
                   </ListGroup.Item>
                 </ListGroup>
                 <ListGroup horizontal>
@@ -350,7 +374,7 @@ export default function CarDetails() {
                     Exterior Color
                   </ListGroup.Item>
                   <ListGroup.Item className="w-50 d-flex justify-content-end">
-                    White
+                    {car.exteriorColor}
                   </ListGroup.Item>
                 </ListGroup>
                 <style jsx global>{`
@@ -366,7 +390,6 @@ export default function CarDetails() {
                   .text-primary {
                     color: var(--secondary-color) !important;
                   }
-                 
                 `}</style>
 
                 <div>
@@ -375,8 +398,7 @@ export default function CarDetails() {
                       <Accordion.Header>Vehicle Description</Accordion.Header>
                       <Accordion.Body>
                         <p>
-                          <BsDashLg/> {car.description}
-                          
+                          <BsDashLg /> {car.description}
                         </p>
                       </Accordion.Body>
                     </Accordion.Item>
@@ -412,39 +434,26 @@ export default function CarDetails() {
               </div>
             </div>
           </div>
-          
+
           <Row className="m-5">
             <Col className="d-flex justify-content-between flex-wrap">
-              <Button 
-              className="mb-3"
-              onClick={showQuoteModal}
-              
-              >
+              <Button className="mb-3" onClick={showQuoteModal}>
                 Request A Quote
               </Button>
 
-              <Button
-              className="mb-3"
-              onClick={showAvailabilityModal}
-              >
+              <Button className="mb-3" onClick={showAvailabilityModal}>
                 Confirm Availability
               </Button>
 
-              <Button 
-              className="mb-3"
-              onClick={showTestDriveModal}
-              >
+              <Button className="mb-3" onClick={showTestDriveModal}>
                 Schedule Test Drive
               </Button>
 
-              <Button 
-              className="mb-3"
-              href="/apply">
+              <Button className="mb-3" href="/apply">
                 Apply Online
               </Button>
             </Col>
           </Row>
-
         </section>
       </main>
     </>
@@ -452,10 +461,5 @@ export default function CarDetails() {
 }
 
 CarDetails.getLayout = function getLayout(page) {
-  return (
-    <Layout>
-      {page}
-    </Layout>
-  )
-}
-
+  return <Layout>{page}</Layout>;
+};
