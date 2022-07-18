0ff4e65116675fab0d540e3ed234f16f05e878e5
Anthony Berrios <54926487+berriosa13@users.noreply.github.com>
2022-07-18 11:32:49 -0400

    added new componenet for phone numbers in forms

---
+++ components/PhoneNumberInput.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+export default function PhoneNumberInput() {
+    const [inputValue, setInputValue] = useState('');
+    const handleInput = (e) => {
+        const formattedPhoneNumber = formatPhoneNumber(e.target.value);
+        setInputValue(formattedPhoneNumber);
+    };
+    return <input className="form-control" onChange={e => handleInput(e)} value={inputValue} name="Cell Phone" placeholder="Cell Phone" />
+}
+
+function formatPhoneNumber(value) {
+    if (!value) return value;
+    const phoneNumber = value.replace(/[^\d]/g, '');
+    const phoneNumberLength = phoneNumber.length;
+    if(phoneNumberLength < 4) return phoneNumber;
+    if(phoneNumberLength < 7)  {
+        return `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3)}`;
+    }
+    return `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3, 6,)}-${phoneNumber.slice(6,10)}`;
+}
