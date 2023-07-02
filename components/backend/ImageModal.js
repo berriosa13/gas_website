ebbd83229cea0fe7fb32761b581649642f96aacf
berriosa13 <berriosa13@gmail.com>
2023-07-02 18:30:13 -0400

    Refactor & impl of sweet alert

--- components/backend/ImageModal.js
+++ components/backend/ImageModal.js
@@ -24,11 +24,15 @@ import { getStorage, ref, deleteObject } from "firebase/storage";
 import ImageDataService from "../../services/images.services";
 import CarDataService from "../../services/cars.services";
 
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
 const ImageModal = ({ setOpenImageModal, setIdForImages, carData }) => {
   const [images, setImages] = useState([]);
   const [currentCar, setCurrentCar] = useState([]);
   const storage = getStorage();
   const carId = carData.id;
+  const MySwal = withReactContent(Swal);
 
   useEffect(() => {
     // Realtime listening of db changes. Updates table as change are made
@@ -43,15 +47,16 @@ const ImageModal = ({ setOpenImageModal, setIdForImages, carData }) => {
       querySnapshot.forEach((doc) => {
         list.push({ id: doc.id, ...doc.data() });
       });
+      console.log("imageList: ", list);
       setImages(list);
     });
 
     const carUnsubscribe = async () => {
-      console.log("carId being passed into doc listener: ", carId);
       const carDocRef = doc(db, "Cars", carId);
       const list = [];
       const carDocSnap = await getDoc(carDocRef);
       list.push({ id: carDocSnap.id, ...carDocSnap.data() });
+      console.log("current car listing: ", list);
       setCurrentCar(list);
     };
 
@@ -61,37 +66,38 @@ const ImageModal = ({ setOpenImageModal, setIdForImages, carData }) => {
     };
   }, [carId]);
 
+  // Function to delete a single image from firebase storage & database
   const deleteImage = async (imageId, imageStorageName, imageUrl) => {
-    const storageRef = ref(storage, `images/${imageStorageName}`);
-    // Check if imageUrl matches thumbnailImage url
-    const thumbnailImage = currentCar?.[0]?.thumbnailImage;
-    console.log("Current car: ", currentCar);
-    if (thumbnailImage === imageUrl) {
-      // Delete thumbnailImage from carDoc
-      try {
+    try {
+      const storageRef = ref(storage, `images/${imageStorageName}`);
+      // Check if imageUrl matches thumbnailImage url
+      const thumbnailImage = currentCar?.[0]?.thumbnailImage;
+      if (thumbnailImage === imageUrl) {
+        // Delete thumbnailImage from carDoc
         await CarDataService.deleteThumbnailImageField(carData.id);
-      } catch (err) {
-        console.log("Error removing thumbnailImage from carDoc ", err);
-        toast.error("Error removing thumbnailImage from carDoc");
       }
-    }
-    // Delete the file
-    deleteObject(storageRef)
-      .then(async () => {
-        // Image file successfully deleted from storage, now delete from image collection
-        console.log("Image (" +imageStorageName+ ") deleted from firebase storage.");
-        try {
-          await ImageDataService.deleteImage(imageId);
-        } catch (err) {
-          console.log("Error deleting image from database ", error);
-          toast.error("Error deleting image from database");
-        }
-      })
-      .catch((error) => {
-        console.log("Error attempting to delete image from storage.");
+      // Delete image from storage
+      deleteObject(storageRef).then(async () => {
+        // Now delete from image collection
+        await ImageDataService.deleteImage(imageId);
       });
-    console.log("Image deleted from firestore collection");
-    toast.success("Image deleted successfully");
+      MySwal.fire({
+        title: "Image deleted👍",
+        icon: "success",
+        timer: 3000,
+        position: "top-end",
+        showConfirmButton: false,
+        timerProgressBar: true,
+      });
+      console.log("Deleted image: " + imageUrl);
+    } catch (e) {
+      console.log("Error attempting to delete image: " + e);
+      MySwal.fire({
+        title: "Error attempting to delete image.",
+        icon: "error",
+        timer: 3000,
+      });
+    }
   };
 
   const setThumbnailImage = async (imageUrl, carId) => {
@@ -99,11 +105,23 @@ const ImageModal = ({ setOpenImageModal, setIdForImages, carData }) => {
     setOpenImageModal(false);
     try {
       await CarDataService.setThumbnailImage(carId, imageUrl);
+      MySwal.fire({
+        title: "Thumbnail set👍",
+        position: "top-end",
+        showConfirmButton: false,
+        icon: "success",
+        timer: 3000,
+        timerProgressBar: true,
+      });
     } catch (err) {
       console.log("Error setting new thumbnailImage ", err);
-      toast.error("Error setting new thumbnailImage");
+      MySwal.fire({
+        title: "Error setting new thumbnail image",
+        position: "top-end",
+        icon: "error",
+        timer: 2000,
+      });
     }
-    toast.success("Image is now being used as a thumbnail");
   };
 
   return (
@@ -121,15 +139,17 @@ const ImageModal = ({ setOpenImageModal, setIdForImages, carData }) => {
         }}
       >
         <Modal.Header closeButton>
+          {" "}
+          <Image
+            src="/imgs/GAS-Icon-Only-2-Color.png"
+            height={93}
+            width={148}
+            alt="GasLogo"
+          />
           <Modal.Title>
-            {" "}
-            <Image
-              src="/imgs/GAS-Icon-Only-2-Color.png"
-              height={93}
-              width={148}
-              alt="GasLogo"
-            />
-            Images for {carData.make} {carData.model} {carData.year}
+            <div className="mx-5">
+              Images for {carData.make} {carData.model} {carData.year}
+            </div>
           </Modal.Title>
         </Modal.Header>
         <GradBar />
@@ -167,7 +187,7 @@ const ImageModal = ({ setOpenImageModal, setIdForImages, carData }) => {
                       <Button
                         className="ms-3"
                         variant="outline-danger"
-                        onClick={(e) =>
+                        onClick={() =>
                           deleteImage(
                             image.id,
                             image.imageStorageName,
@@ -205,7 +225,7 @@ const ImageModal = ({ setOpenImageModal, setIdForImages, carData }) => {
           </Container>
         </Modal.Body>
         <GradBar />
-        <Modal.Footer className="d-flex justify-content-center">
+        <Modal.Footer className="d-flex flex-column justify-content-center">
           <Image
             className=""
             src="/imgs/GAS-Logo-text.png"
